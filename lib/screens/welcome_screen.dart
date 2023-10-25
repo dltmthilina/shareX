b27f2b50caee3c1b78f6824d6845bc8730f880f1
dltmthilina <dltmthilinamaduranga@gmail.com>
2023-10-25 14:07:45 +0530

    create register page and connect firebase

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:shareex/screens/registration_screen.dart';
+import 'package:shareex/widgets/custom_button.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("assets/image1.png",
+                height: 400 ,
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "Let's get started",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "Never a better time than now to start.",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black38,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: CustomButton(text: "Get Started", 
+                  onPressed: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context)=>const RegistrationScreen()
+                        ),
+                     );
+                  }
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+        ),
+    );
+  }
+ 
+
+}
+
