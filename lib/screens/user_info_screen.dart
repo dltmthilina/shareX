74f52c5a62ebf3cbc7da00df0203f216c456b582
dltmthilina <dltmthilinamaduranga@gmail.com>
2023-10-29 02:48:49 +0530

    create user_info_screen

--- lib/screens/user_info_screen.dart
+++ lib/screens/user_info_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+
+import 'package:shareex/widgets/custom_button.dart';
 
 class UserInfoScreen extends StatefulWidget {
   const UserInfoScreen({super.key});
@@ -8,10 +11,129 @@ class UserInfoScreen extends StatefulWidget {
 }
 
 class _UserInfoScreenState extends State<UserInfoScreen> {
+  File? image;
+  final nameController = TextEditingController();
+  final homeTownController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameController.dispose();
+    homeTownController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("User Information"),
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 5.0),
+            child: Center(
+              child: Column(
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: image == null
+                        ? const CircleAvatar(
+                            backgroundColor: Colors.purple,
+                            radius: 50,
+                            child: Icon(
+                              Icons.account_circle,
+                              size: 50,
+                              color: Colors.white,
+                            ),
+                          )
+                        : CircleAvatar(
+                            backgroundImage: FileImage(image!),
+                            radius: 50,
+                          ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+                    margin: const EdgeInsets.only(top: 20),
+                    child: Column(
+                      children: [
+                        textFeld(
+                          hintText: "John Smith",
+                          icon: Icons.account_circle,
+                          inputType: TextInputType.name,
+                          maxLines: 1,
+                          controller: nameController,
+                        ),
+                        textFeld(
+                          hintText: "Enter your home town",
+                          icon: Icons.edit,
+                          inputType: TextInputType.name,
+                          maxLines: 2,
+                          controller: homeTownController,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width * 0.80,
+                    child: CustomButton(text: "Continue", onPressed: () {}),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget textFeld({
+    required String hintText,
+    required IconData icon,
+    required TextInputType inputType,
+    required int maxLines,
+    required TextEditingController controller,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: TextFormField(
+        cursorColor: Colors.purple,
+        controller: controller,
+        keyboardType: inputType,
+        maxLines: maxLines,
+        decoration: InputDecoration(
+          prefixIcon: Container(
+            margin: const EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.purple,
+            ),
+            child: Icon(
+              icon,
+              size: 20,
+              color: Colors.white,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(
+              color: Colors.transparent,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(
+              color: Colors.transparent,
+            ),
+          ),
+          hintText: hintText,
+          alignLabelWithHint: true,
+          border: InputBorder.none,
+          fillColor: Colors.purple.shade50,
+          filled: true,
+        ),
+      ),
     );
   }
 }
