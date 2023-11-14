d05766717871d703bd710901a4483a878337da0b
dltmthilina <dltmthilinamaduranga@gmail.com>
2023-11-14 13:40:47 +0530

    Create function to capitalize first letter

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -24,3 +24,15 @@ Future<File?> pickImage(BuildContext context) async {
   }
   return image;
 }
+
+String capitalizeAllWord(String value) {
+  var result = value[0].toUpperCase();
+  for (int i = 1; i < value.length; i++) {
+    if (value[i - 1] == " ") {
+      result = result + value[i].toUpperCase();
+    } else {
+      result = result + value[i];
+    }
+  }
+  return result;
+}
